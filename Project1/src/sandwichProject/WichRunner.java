6d8aec5c6137cc1f9f7a34b6e9dd0c930d97a066
spatra12 <PatraS4337@sduhsd.lan>
2016-05-10 10:06:05 -0700

    update

--- Project1/src/sandwichProject/WichRunner.java
+++ Project1/src/sandwichProject/WichRunner.java
@@ -1,5 +1,16 @@
 package sandwichProject;
 
+import java.util.Scanner;
+
 public class WichRunner {
+	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in); 
+		
+		System.out.println("THIS IS A SANDWICH MAKER!!!\nWould you like to make a sandwich? (y/n)");
+		
+		//if((input.next().equals("N") && input.next().equals("n"))
+	}
 	
 }
